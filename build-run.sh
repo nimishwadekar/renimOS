36e7e36f3fc34b15420c7b9f4cdc93e9af83bc9b
Nimish Wadekar <wadekar.nimish@gmail.com>
2022-10-27 21:59:39 +0800

    Fix build command

--- build-run.sh
+++ build-run.sh
@@ -7,15 +7,15 @@ fi
 
 firmware=$1
 if [ "$firmware" = "bios" ]; then
-    cargo build --target=x86_64-eden.json -Zbuild-std=core,compiler_builtins -Zbuild-std-features=compiler-builtins-mem
-    cargo run -p boot
+    cargo build --target=x86_64-eden.json -Zbuild-std=core,compiler_builtins -Zbuild-std-features=compiler-builtins-mem && \
+    cargo run -p boot && \
     qemu-system-x86_64 -drive format=raw,file=target/x86_64-eden/debug/boot-bios-eden.img
     exit 0
 fi
 
 if [ "$firmware" = "uefi" ]; then
-    cargo build --target=x86_64-eden.json -Zbuild-std=core,compiler_builtins -Zbuild-std-features=compiler-builtins-mem
-    cargo run -p boot
+    cargo build --target=x86_64-eden.json -Zbuild-std=core,compiler_builtins -Zbuild-std-features=compiler-builtins-mem && \
+    cargo run -p boot && \
     qemu-system-x86_64 -bios boot/OVMF.fd -drive format=raw,file=target/x86_64-eden/debug/boot-uefi-eden.img
     exit 0
 fi
