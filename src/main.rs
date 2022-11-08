8821e98673f02dec6f3b64aa4abf34ba23773b5f
Nimish Wadekar <wadekar.nimish@gmail.com>
2022-11-08 17:34:52 +0800

    Refine main

--- src/main.rs
+++ src/main.rs
@@ -2,32 +2,33 @@
 #![no_main]
 
 use bootloader::{entry_point, BootInfo};
+use display::init_display;
 
+mod spinlock;
 mod serial;
-mod fonts;
+mod display;
+mod arch;
+mod interrupts;
 
 entry_point!(kernel_main);
 
 #[no_mangle]
 fn kernel_main(boot_info: &'static mut BootInfo) -> ! {
-    let framebuffer = boot_info.framebuffer.as_mut().unwrap();
-
-    framebuffer.buffer_mut()[0] = 0xFF;
-    framebuffer.buffer_mut()[1] = 0xFF;
-    framebuffer.buffer_mut()[2] = 0xFF;
-    framebuffer.buffer_mut()[3] = 0xFF;
-
     serial_println!("Hello Eden!");
-    //serial_println!("{:#?}", boot_info);
+    serial_println!("{:#?}", boot_info);
 
-    let psf = fonts::PSF::parse(include_bytes!("fonts/files/zap-light16.psf")).unwrap();
-    serial_println!("psf loaded");
+    if !init_display(boot_info.framebuffer.as_ref()) {
+        serial_println!("Framebuffer not initialised");
+    }
+
+    println!("Hello kernel");
 
     loop {}
 }
 
 
 #[panic_handler]
-fn panic(_info: &core::panic::PanicInfo) -> ! {
+fn panic(info: &core::panic::PanicInfo) -> ! {
+    serial_println!("{}", info);
     loop {}
 }
