15e659d5f28f4485a7948c3267cb9c5e51150f98
Nimish Wadekar <wadekar.nimish@gmail.com>
2022-11-08 17:33:48 +0800

    Implement interrupt functions

---
+++ src/interrupts.rs
@@ -0,0 +1,13 @@
+use crate::arch::InterruptFunctions;
+
+pub fn disable() {
+    crate::arch::interrupts::x86_64::disable();
+}
+
+pub fn enable() {
+    crate::arch::interrupts::x86_64::enable();
+}
+
+pub fn are_enabled() -> bool {
+    crate::arch::interrupts::x86_64::are_enabled()
+}
